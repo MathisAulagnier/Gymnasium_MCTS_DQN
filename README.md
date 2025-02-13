e0bcff5cc093f568d6895b06936a35d426d1179b
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2025-02-13 17:41:39 -0500

    création du env

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Gymnasium_MCTS_DQN
+
 🌀 Projet 8INF974 - Agents intelligents avec Gymnasium 🎮  Implémentation de deux approches d'apprentissage par renforcement pour résoudre des jeux Atari : 
+
 1. Monte Carlo Tree Search (MCTS) avec exploration UCB1 🌳
 2. Deep Q-Learning (DQN) avec expérience replay et CNN 🧠
+
+# Installation
+
+1. Cloner le dépôt
+2. Installer les dépendances : `pip install -r requirements.txt`
---
+++ src/env.py
@@ -0,0 +1,37 @@
+import os
+import gymnasium as gym
+
+import ale_py 
+
+from stable_baselines3 import A2C
+
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack, VecTransposeImage, VecVideoRecorder
+
+
+
+import ale_py
+
+gym.register_envs(ale_py)
+
+
+environment_name = 'ALE/VideoChess-v5'
+
+env = gym.make(environment_name, render_mode='human')
+
+
+
+episodes = 1
+for episode in range(1, episodes+1):
+    state = env.reset()
+    terminated = False
+    score = 0
+
+    while not terminated:
+        env.render() 
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        score += reward
+    print(f'Episode: {episode}, Score: {score}')
+env.close()
