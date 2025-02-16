1886b2f5eaae7b2621977bdc98786e59b9cd1c90
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2025-02-16 17:50:12 -0500

    ne pas utiliser MCTS

---
+++ src/mcts/1.py
@@ -0,0 +1,38 @@
+import numpy as np
+from collections import defaultdict
+import math
+
+from NODE import Node
+from MCTS import MCTS
+
+import ale_py
+import os
+import gymnasium as gym
+
+
+def play_game_with_mcts(env, mcts, episodes=1):
+    for episode in range(episodes):
+        state = env.reset()
+        done = False
+        total_reward = 0
+        
+        while not done:
+            action = mcts.get_best_action(state[0])  # state[0] contains the actual observation
+            state, reward, terminated, truncated, _ = env.step(action)
+            total_reward += reward
+            done = terminated or truncated
+            
+        print(f"Episode {episode + 1}: Total Reward = {total_reward}")
+
+
+gym.register_envs(ale_py)
+
+# Initialiser l'environnement
+environment_name = 'ALE/VideoChess-v5'
+env = gym.make(environment_name, render_mode='human')
+
+# Créer l'instance MCTS
+mcts = MCTS(env)
+
+# Jouer quelques parties
+play_game_with_mcts(env, mcts, episodes=3)
