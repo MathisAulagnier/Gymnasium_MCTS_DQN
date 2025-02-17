d982b05e730a45c6234e68672356892ee9407fd0
mathisaulagnier <mathis.aulagnier@telecomnancy.eu>
2025-02-17 16:35:10 -0500

    clear

--- src/mcts/NODE.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, state, parent=None, action=None):
-        self.state = state
-        self.parent = parent
-        self.action = action
-        self.children = []
-        self.wins = 0
-        self.visits = 0
-        self.untried_actions = None
